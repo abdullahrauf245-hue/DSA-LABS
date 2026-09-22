c404215131a084afe124058a8923d56291c6e676
Muhammad Abdullah <abdullahrauf245@gmail.com>
2026-09-22 10:52:49 +0500

    Addin methods in task 2 string pool

--- Lab 3/task2stringpool.cpp
+++ Lab 3/task2stringpool.cpp
@@ -14,6 +14,28 @@ public:
         currentSize = 0;
         stringPool = new string[maxSize];
     }
+
+    // adds a string to the pool
+    void addString(string value) {
+        if (currentSize < maxSize) {
+            stringPool[currentSize] = value;
+            currentSize++;
+            cout << "Added: " << value << endl;
+        } else {
+            cout << "Pool is full, cannot add " << value << endl;
+        }
+    }
+
+    // removes a string from the pool WITHOUT freeing memory
+    void removeString(int index) {
+        if (index >= 0 && index < currentSize) {
+            cout << "Removed (without freeing): " << stringPool[index] << endl;
+            // notice: nothing is cleared or deleted here on purpose
+            // this leaves behind a memory leak that we deal with later
+        } else {
+            cout << "Invalid index." << endl;
+        }
+    }
 };
 
 int main() {
@@ -22,6 +44,14 @@ int main() {
     cout << "Pool created." << endl;
     cout << "maxSize: " << pool.maxSize << endl;
     cout << "currentSize: " << pool.currentSize << endl;
+
+    cout << endl;
+    pool.addString("apple");
+    pool.addString("banana");
+    pool.addString("cherry");
+
+    cout << endl;
+    pool.removeString(1);
 
     return 0;
 }
